e9960090a784bd13818a3405b59eba9cec079148
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-06-22 16:09:02 -0300

    feat: setup todo-comments and nightfox theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,46 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require("nightfox").setup {
+        options = {
+          colorblind = {
+            enable = true,
+            severity = {
+              protan = 1,
+              deutan = 0.8,
+            },
+          },
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+          modules = {
+            aerial = true,
+            cmp = true,
+            ["dap-ui"] = true,
+            dashboard = true,
+            diagnostic = true,
+            gitsigns = true,
+            native_lsp = true,
+            neotree = true,
+            notify = true,
+            symbol_outline = true,
+            telescope = true,
+            treesitter = true,
+            whichkey = true,
+          },
+        },
+      }
+    end,
+  }, -- lazy
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "BufRead",
+    config = function() require("todo-comments").setup() end,
+  },
 }
