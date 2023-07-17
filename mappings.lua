a1fd485ac177a1833b8069c498817f561484adea
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-07-17 19:50:28 -0300

    feat: added mapping for quality of coding

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -20,9 +22,30 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- 0 is now ^
+    ["0"] = { "^" },
+    -- Center the cursor after scrolling
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+    -- Center the cursor after search
+    ["n"] = { "nzzzv" },
+    ["N"] = { "Nzzzv" },
+    -- Custom binds for makefile
+    ["<leader>m"] = { name = " Makefile" },
+    ["<leader>mm"] = { "<cmd>1TermExec cmd='make' dir=./<cr>", desc = "make" },
+    ["<leader>mr"] = { "<cmd>1TermExec cmd='make run' dir=./<cr>", desc = "make run" },
+    ["<leader>mv"] = { "<cmd>1TermExec cmd='make runv' dir=./<cr>", desc = "make runv" },
+    ["<leader>mc"] = { "<cmd>1TermExec cmd='make clean' dir=./<cr>", desc = "make clean" },
+    ["<leader>mf"] = { "<cmd>1TermExec cmd='make fclean' dir=./<cr>", desc = "make fclean" },
+    ["<leader>mR"] = { "<cmd>1TermExec cmd='make re' dir=./<cr>", desc = "make re" },
+    ["<leader>md"] = { "<cmd>1TermExec cmd='make debug' dir=./<cr>", desc = "make debug" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  i = {
+    -- Insert tab char
+    --["<C-l>"] = { "<C-v><Tab>" },
   },
 }
