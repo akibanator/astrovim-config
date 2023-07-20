4fd74c2e7c7d6fe722b905072ccd2d80a030b5a9
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2023-07-20 17:36:06 -0300

    feat: input args when c debugging

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -25,12 +25,45 @@ return {
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-      })
-    end,
+    opts = {
+      handlers = {
+        function(config)
+          -- all sources with no handler get passed here
+          -- Keep original functionality
+          require("mason-nvim-dap").default_setup(config)
+        end,
+        cppdbg = function(config)
+          config.configurations = {
+            {
+              name = "Launch file",
+              type = "cppdbg",
+              request = "launch",
+              args = function()
+                local args_string = vim.fn.input "Input arguments: "
+                return vim.split(args_string, " ")
+              end,
+              program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+              cwd = "${workspaceFolder}",
+              stopAtEntry = true,
+            },
+            {
+              name = "Attach to gdbserver :1234",
+              type = "cppdbg",
+              request = "launch",
+              MIMode = "gdb",
+              miDebuggerServerAddress = "localhost:1234",
+              miDebuggerPath = "/usr/bin/gdb",
+              cwd = "${workspaceFolder}",
+              args = function()
+                local args_string = vim.fn.input "Input arguments: "
+                return vim.split(args_string, " ")
+              end,
+              program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+            },
+          }
+          require("mason-nvim-dap").default_setup(config)
+        end,
+      },
+    },
   },
 }
