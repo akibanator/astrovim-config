61e42cb8a3698496a92b5acd40523dfeb5f7c567
Ailton Kenji Akiba <aakiba@kenji-pc.lan>
2023-08-02 18:46:14 -0300

    feat: set transparent to true

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,6 +14,7 @@ return {
     config = function()
       require("nightfox").setup {
         options = {
+          transparent = true,
           colorblind = {
             enable = true,
             severity = {
